eb505dfdf3b345ad897f5d40e51ec005787e9924
tnguyen449 <tnguyen.5593>
2017-07-20 02:15:39 +0700

    delete old refactored files
    Signed-off-by: tnguyen449 <tnguyen.5593>

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('refactor', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
